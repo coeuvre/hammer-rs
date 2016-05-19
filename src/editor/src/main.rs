c65d617192627d2ea01a6a8fa8d1a140e2a88a0a
Coeuvre Wong <coeuvre@gmail.com>
2016-05-20 00:43:58 +0100

    Load opengl

--- src/editor/src/main.rs
+++ src/editor/src/main.rs
@@ -1,29 +1,47 @@
+extern crate gl;
+
 #[macro_use]
 extern crate log;
 extern crate env_logger;
 
 extern crate winapi;
+extern crate gdi32;
 extern crate kernel32;
+extern crate opengl32;
 extern crate user32;
 
 use std::mem;
+use std::ffi::{CString, CStr};
 
-use winapi::*;
+use winapi::basetsd::*;
+use winapi::c_int;
+use winapi::minwindef::*;
+use winapi::windef::*;
+use winapi::wingdi::*;
+use winapi::winnt::*;
+use winapi::winuser::*;
+use gdi32::*;
 use kernel32::*;
+use opengl32::*;
 use user32::*;
+
+pub type Error = Box<std::error::Error + Send + Sync>;
 
 macro_rules! wstr {
     ($str:expr) => ({
         use std::ffi::OsStr;
         use std::os::windows::ffi::OsStrExt;
-        OsStr::new($str).encode_wide()
-                        .chain(Some(0).into_iter())
-                        .collect::<Vec<_>>()
+        OsStr::new($str)
+            .encode_wide()
+            .chain(Some(0).into_iter())
+            .collect::<Vec<_>>()
     });
 }
 
 pub struct Window {
     hwnd: HWND,
+
+    context: RenderContext,
 
     x: i32,
     y: i32,
@@ -34,6 +52,81 @@ pub struct Window {
 impl Window {
 }
 
+pub struct RenderContext {
+    lib: HMODULE,
+    hdc: HDC,
+    hglrc: HGLRC,
+    _pfd: PIXELFORMATDESCRIPTOR,
+}
+
+impl RenderContext {
+    pub unsafe fn new(hdc: HDC) -> Result<RenderContext, Error> {
+        let name = wstr!("opengl32.dll");
+        let lib = LoadLibraryW(name.as_ptr());
+        if lib != 0 as HMODULE {
+            let mut pfd: PIXELFORMATDESCRIPTOR = mem::zeroed();
+            pfd.nSize = mem::size_of_val(&pfd) as WORD;
+            pfd.nVersion = 1;
+            pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
+            pfd.iPixelType = PFD_TYPE_RGBA;
+            pfd.cColorBits = 32;
+            pfd.cDepthBits = 24;
+            pfd.cStencilBits = 8;
+            pfd.iLayerType = PFD_MAIN_PLANE;
+
+            let pfi = ChoosePixelFormat(hdc, &mut pfd);
+            if pfi != 0 {
+                if SetPixelFormat(hdc, pfi, &mut pfd) != 0 {
+                    let hglrc = wglCreateContext(hdc);
+
+                    if hglrc != 0 as HGLRC {
+                        Ok(RenderContext {
+                            lib: lib,
+                            hdc: hdc,
+                            hglrc: hglrc,
+                            _pfd: pfd,
+                        })
+                    } else {
+                        Err(From::from("Failed to create OpenGL context"))
+                    }
+                } else {
+                    Err(From::from("Failed to set pixel format"))
+                }
+            } else {
+                Err(From::from("Failed to choose pixel format"))
+            }
+        } else {
+            Err(From::from("Failed to load opengl32.dll"))
+        }
+    }
+
+    pub unsafe fn make_current(&self) -> Result<(), Error> {
+        if wglGetCurrentContext() != self.hglrc {
+            if wglMakeCurrent(self.hdc, self.hglrc) != 0 {
+                gl::load_with(|symbol| {
+                    let cstr = CString::new(symbol).unwrap();
+                    let mut ptr = wglGetProcAddress(cstr.as_ptr());
+                    if ptr == 0 as PROC {
+                        ptr = GetProcAddress(self.lib, cstr.as_ptr());
+                    }
+                    ptr
+                });
+                let version = CStr::from_ptr(mem::transmute(gl::GetString(gl::VERSION)));
+                println!("OpenGL Version: {:?}", version);
+                Ok(())
+            } else {
+                Err(From::from("Failed to make context be current"))
+            }
+        } else {
+            Ok(())
+        }
+    }
+
+    pub unsafe fn swap_buffers(&self) {
+        SwapBuffers(self.hdc);
+    }
+}
+
 #[allow(unused_variables)]
 unsafe extern "system"
 fn window_proc(hwnd: HWND, msg: UINT, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
@@ -41,7 +134,7 @@ fn window_proc(hwnd: HWND, msg: UINT, wparam: WPARAM, lparam: LPARAM) -> LRESULT
 
     match msg {
         WM_CREATE => {
-            let cs = &*(lparam as (*const CREATESTRUCTW));
+            let cs = &*(lparam as (*mut CREATESTRUCTW));
             let window = &mut *(cs.lpCreateParams as (*mut Window));
             SetWindowLongPtrW(hwnd, 0, window as (*mut Window) as LONG_PTR);
 
@@ -55,12 +148,28 @@ fn window_proc(hwnd: HWND, msg: UINT, wparam: WPARAM, lparam: LPARAM) -> LRESULT
             window.w = rect.right - rect.left;
             window.h = rect.bottom - rect.top;
 
-            SetWindowLongPtrW(hwnd, GWL_STYLE, winuser::WS_POPUP as LONG_PTR);
+            SetWindowLongPtrW(hwnd, GWL_STYLE, WS_POPUP as LONG_PTR);
             //SetWindowLongPtrW(hwnd, GWL_EXSTYLE, winuser::WS_EX_LAYERED as LONG_PTR);
+
+            window.context = RenderContext::new(GetDC(hwnd)).unwrap();
+        }
+
+        WM_PAINT => {
+            window.context.make_current().unwrap();
+            gl::ClearColor(0.3, 0.3, 0.3, 1.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+            window.context.swap_buffers();
         }
 
         WM_CLOSE => {
             PostQuitMessage(0);
+        }
+
+        WM_KEYDOWN => {
+            let key = wparam as c_int;
+            if key == VK_ESCAPE {
+                PostQuitMessage(0);
+            }
         }
 
         _ => {
