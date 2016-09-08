1ab58be5f400aaa48dbf914f7a4c04d7f743115d
Coeuvre Wong <coeuvre@gmail.com>
2016-09-09 00:00:23 +0100

    Restructure

--- src/core/build.rs
+++ src/core/build.rs
@@ -1,6 +1,6 @@
 extern crate gcc;
 
 fn main() {
-    gcc::compile_library("libstb_image.a", &["src/util/stb_image.c"]);
+    gcc::compile_library("libstb_image.a", &["util/stb_image.c"]);
     println!("cargo:rustc-link-lib=static=stb_image");
 }
